9cb13e8bae484697c398bc3d6ff4973b376cb6a5
unnath123 <unnathpgowda@gmail.com>
2023-10-06 01:38:35 +0530

    distance and price breakdown

--- SearchScript.js
+++ SearchScript.js
@@ -31,12 +31,54 @@ function CalculateDistance(listingLocation){
         return distance;
 }
 
+
+function showBookingCostBreakdown(listing){
+  // listing={price:{
+  //   rate:100
+  // }}
+  const additionalFees = listing.rate * 0.10; // Assuming additional fees are 10% of base price
+  const totalCost = listing.rate + additionalFees;
+
+  // Create a modal dialog box
+  const modal = document.createElement("div");
+  modal.style.display = "block";
+  modal.style.width = "300px";
+  modal.style.height = "200px";
+  modal.style.backgroundColor = "#fff";
+  modal.style.position = "fixed";
+  modal.style.top = "50%";
+  modal.style.left = "50%";
+  modal.style.transform = "translate(-50%, -50%)";
+  modal.style.padding = "20px";
+  modal.style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.1)";
+  modal.style.borderRadius = "10px"
+
+  // Add booking cost breakdown to the modal
+  modal.innerHTML = `
+      <h2>Booking Cost Breakdown</h2>
+      <p>Base Rate: $${listing.rate.toFixed(2)}</p>
+      <p>Additional Fees: $${additionalFees.toFixed(2)}</p>
+      <p>Total Cost: $${totalCost.toFixed(2)}</p>
+  `;
+
+  // Add a close button to the modal
+  const closeButton = document.createElement("button");
+  closeButton.innerText = "Close";
+  closeButton.addEventListener("click", () => modal.style.display = "none");
+  modal.appendChild(closeButton);
+
+  // Add the modal to the body
+  document.body.appendChild(modal);
+}
+
+
 function CreateListCards() {
   const listing = document.getElementById("Listings");
 
   for (let i = 0; i < 5; i++) {
     const listingLocation = `${arr[i].lat},${arr[i].lng}`;
-    const distance = CalculateDistance(listingLocation);
+    const distance = 20
+    // CalculateDistance(listingLocation);
 
     const block = document.createElement("div");
     block.className = "block1";
@@ -60,11 +102,13 @@ function CreateListCards() {
                                 <div class="listingDivider"></div>
                                 <div class="desc3">
                                     <div class="desc3-one"><p>${arr[i].rating}<i class="fa-solid fa-star" style="color: #fddc08;"></i> (${arr[i].reviewsCount}reviews)</p></div>
-                                    <div class="desc3-two"><p>${arr[i].price.rate}$ / night</p></div>
+                                    <div class="desc3-two"><button class="costBDn" onclick='showBookingCostBreakdown(${JSON.stringify(arr[i].price)})'>${(arr[i].price.rate)*80}rs / night</button></div>
                                 </div>
                             </div>
                             
                             `;
+                  
+  
 
     // render marker on map for the location
     let pos ={ lat:  arr[i].lat, lng: arr[i].lng };
@@ -109,7 +153,7 @@ async function renderMarker(pos){
     const marker1 = new AdvancedMarkerElement({
         map: map,
         position: pos,
-        title: "stay",
+        title: "stay", 
       });
 }
 
